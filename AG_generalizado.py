69fb42c6893c67cb8a516a23610ff575cdd9ff68
Gerardo Sánchez Ortega <gerardo131.gs@gmail.com>
2016-08-22 17:38:04 -0500

    Abilitado para minimizar funcion cross in trayn

--- AG_generalizado.py
+++ AG_generalizado.py
@@ -89,7 +89,7 @@ def fobj(X):
 	return suma
 	#------------------------------------------------
 """
-
+"""
 	#------------ funcion Beale -----------------
 def fobj(X):
 	var=[]
@@ -97,6 +97,14 @@ def fobj(X):
 		var.append( float(dCodBin(X[i])) )
 	return (1.5-var[0]+var[0]*var[1])**2 + (2.25-var[0]+var[0]*var[1]**2)**2 + (2.625-var[0]+var[0]*var[1]**3)**2
 	#------------------------------------------------
+"""
+	#------------- funcion Cross in trayn --------
+def fobj(X):
+	var=[]
+	for i in xrange(0,len(X)):
+		var.append( float(dCodBin(X[i])) )
+	return -0.0001*( abs( math.sin(var[0])*math.sin(var[1])*math.exp(abs(100.0-math.sqrt(var[0]**2+var[1]**2)/math.pi ) ) ) +1)**(0.1)
+	#---------------------------------------------
 
 # funcion de adaptacion 
 def adap(X,fmax,fmin):
