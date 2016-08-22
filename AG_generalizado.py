316ec6330633830507716ea82c832fe39a840b29
Gerardo Sánchez Ortega <gerardo131.gs@gmail.com>
2016-08-22 11:52:12 -0500

    soporte para numeros punto flotante

--- AG_generalizado.py
+++ AG_generalizado.py
@@ -6,26 +6,31 @@ import matplotlib.pyplot as plt
 
 #---------------- Codificacion ---------------------------------
 
+
 def codBin(num, preci):
+	Inter=10
 	binNum=""
 	Bin=""
+
 	if num <0:
 		Bin+="1"
-		binNum=bin(num)[3:]
+		binNum=bin(int( (-1.0*float(num)*(2**(preci+1.0)-1.0 ) )/(Inter) ) )[2:] 
 	else:
 		Bin+="0"
-		binNum=bin(num)[2:]
+		binNum=bin(int( ( (num)*(2**(preci+1.0)-1.0 ) )/(Inter) ))[2:]
 	for i in xrange(0,preci-len(binNum)):
 		Bin+="0"
 	Bin += binNum
 	return list(Bin)
 	
 def dCodBin(Bin):
-	
+	Inter =10
+	bi=int(''.join(Bin[1:]),2)
+	num =( (float(bi)* float(Inter))/float(2**len(Bin)-1.0)  )
 	if Bin[0]=="1":
-		return -int(''.join(Bin[1:]),2)
+		return -num
 	else:
-		return int(''.join(Bin[1:]),2)   
+		return num 
 
 #--------------------- Poblacion inicial ------------------------
 def PoInAl(nind, nvar, preci):
@@ -53,12 +58,15 @@ def fobj(X):
 	
 	res=0
 	preres = -np.exp(0.5*(np.cos(2.0*np.pi*var[0])+np.cos(2.0*np.pi*var[1]) ) )+np.exp(1)+20.0
-	res= -20.0*np.exp( -0.2*np.sqrt(0.5*(var[0]**2+var[1]**2) ) ) + preres
+	res = -20.0*np.exp( -0.2*np.sqrt(0.5*(var[0]**2+var[1]**2) ) ) + preres
 	return res
+	#------------------------------------------------
 
+# funcion de adaptacion -------------------------------------------
 def adap(X,fmax,fmin):
 	return fmax + fmin - fobj(X)
 
+#busqueda de genotipo con valor de funcion objetivo maximo y minimo
 def adapMaxMin(pob):
 	aux =[]
 	for i in xrange(0,len(pob)):
@@ -77,7 +85,10 @@ def cal(pob):
 		print str(adap(pob[i],fmax,fmin) ) +"  "+str(fobj(pob[i])) +"  "+ str(dCodBin(pob[i][0])) +"  "+ str(dCodBin(pob[i][1])) 
 	print "----------------"
 	for i in xrange(0,len(pob) ):
-		val.append( float(adap(pob[i],fmax,fmin))/float(aco) )
+			if aco ==0 :
+				val.append(0.0)
+			else:
+				val.append( float(adap(pob[i],fmax,fmin))/float(aco) )
 	return val
 
 def linRank(pob,Mu):
@@ -104,8 +115,18 @@ def linRank(pob,Mu):
 		proba.append(val[i])
 	return proba
 
-
-
+#ordena la poblacion en funcion de la calificacion de cada genotipo 
+def ordenar (cal,pob):
+	var = []
+	varC = []
+	aux = []
+	for i in xrange (0,len(pob)):
+		aux.append([fobj(pob[i]),i])
+	aux.sort(reverse=True)
+	for i in xrange (0,len(pob)):
+		var.append(pob[aux[i][1]])
+		varC.append( cal[aux[i][1]] )
+	return var,varC
 #----------------------- seleccion -------------------------------
 
 def selec(cal,pob):
@@ -156,21 +177,21 @@ def mutacion (pob):
 
 
 def main():
-	NIND =100
-	MAXGE = 500
+	NIND =6
+	MAXGE = 3
 	NVAR = 2
-	PRECI= 3
-	
+	PRECI= 17
+
 	# seleccionar poblacion inicial 
-	
 	pob=PoInAl(NIND, NVAR, PRECI)
 
 	gen = 0 
 	while(gen<MAXGE):
 		newPob=[]
-		
+		calificacion = cal(pob)
+		pob,calificacion =ordenar(calificacion,pob)
 		for i in xrange(0,NIND/2):
-			calificacion = cal(pob)
+		
 			can1,can2=selec(calificacion,pob)
 			canM1,canM2=mezclar(pob[can1],pob[can2])
 			newPob.append(canM1)
@@ -181,12 +202,21 @@ def main():
 	max=0
 	ind=0
 	fmax,fmin = adapMaxMin(pob)
-	for i in xrange(0,NIND):
+	for i in xrange(0,len(pob)):
 		if max <adap(pob[i],fmax,fmin) :
 			max = adap(pob[i],fmax,fmin)
 			ind=i
 	print " f("+ str(dCodBin(pob[ind][0]) ) +","+str(dCodBin(pob[ind][1]) )+") = " +str(fobj(pob[ind])) 
 	print len(pob)
-main()
+#main()
+f = -5.0
+while (f<5.0):
+	print "-------------------"
+	a =  codBin(f,17)
+	print a
+	print f
+	print dCodBin(a)
+	print "-------------------"
+	f += 0.1
 #print fobj(["11","11"])
 #print codBin(8,3)
