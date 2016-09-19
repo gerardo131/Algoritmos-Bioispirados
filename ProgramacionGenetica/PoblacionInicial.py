86546e5910421c73896bac8b8ffd82ed1e83751f
Gerardo Sánchez Ortega <gerardo131.gs@gmail.com>
2016-09-19 13:51:05 -0500

    V0.1

--- ProgramacionGenetica/PoblacionInicial.py
+++ ProgramacionGenetica/PoblacionInicial.py
@@ -1,5 +1,12 @@
 import numpy as np
+import Posfija
 
-calificacion = 0
-genotipo = []
+def aleatorio( n=2, prof=0 ):
+	poblacion = []
+	for i in xrange(0,n) :
+		poblacion.append( Posfija.crear(prof) )
+	return poblacion
 
+"""
+print aleatorio(2,3)
+"""
